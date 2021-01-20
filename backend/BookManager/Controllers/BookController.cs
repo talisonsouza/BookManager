fab60ffb8298d1e8902474a0218ea3dfd4153d66
talisonsouza <rodrigo.talisson@gmail.com>
2021-01-20 15:07:14 -0300

    separating backend into folder

---
+++ backend/BookManager/Controllers/BookController.cs
@@ -0,0 +1,73 @@
+﻿using BookManager.Entities;
+using BookManager.Handles;
+using BookManager.Models;
+using BookManager.Models.Book;
+using BookManager.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookRepository _repository;
+        private readonly BookHandle _handler;
+
+        public BookController(IBookRepository repository, BookHandle handle)
+        {
+            _repository = repository;
+            _handler = handle;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int Id = 0)
+        {
+            object book = null;
+
+            if (Id > 0)            
+                book = await _repository.GetAsync(Id);            
+            else            
+                book = await _repository.GetAsync();
+
+            if(book != null)return Ok(book);
+            
+            return NotFound();
+        }
+
+        [HttpPost]
+        public CommandResult Index([FromBody]CreateBookModel model)
+        {
+            try
+            {
+                return _handler.Create(model);
+            }
+            catch (Exception ex)
+            {
+                return new CommandResult { Success = false, Message = ex.Message };
+            } 
+        }
+
+        [HttpDelete]
+        public CommandResult Delete(int id)
+        {
+            try
+            {
+                _repository.Delete(id);
+                return new CommandResult
+                {
+                    Success = true,
+                    Message = "Registro deletado com sucesso!"
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CommandResult { Success = false, Message = ex.Message };
+            }
+        }
+
+    }
+}
---
+++ backend/BookManager/Entities/Editor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookManager.Entities
+{
+    public class Editor
+    {
+        public int EditorId { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+    }
+}
